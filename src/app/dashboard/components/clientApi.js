6adab9e1d852ca06282542fc727307c8c7e8107c
piasmajumdar <piasmajumdar@gmail.com>
2026-09-05 19:17:38 +0600

    Implemented JWT

--- src/app/dashboard/components/clientApi.js
+++ src/app/dashboard/components/clientApi.js
@@ -1,11 +1,20 @@
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_SERVER_URL
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_SERVER_URL;
 
 export async function clientApi(path, options = {}) {
+  const { data: tokenData } = await authClient.token();
+
   const response = await fetch(`${API_URL.replace(/\/$/, "")}${path}`, {
     ...options,
     headers: {
       "Content-Type": "application/json",
       ...(options.headers || {}),
+      ...(tokenData?.token
+        ? { Authorization: `Bearer ${tokenData.token}` }
+        : {}),
     },
   });
 
